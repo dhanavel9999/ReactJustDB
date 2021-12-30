66ce78fcd88ba6918ee66c93ed42698e6fb557d4
dhanavel9999 <dhanavel9999@gmail.com>
2021-12-30 21:43:23 +0530

    API interfacing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import Op from './HookPrac/hook1';
+
 import DispTbl from './HookPrac/fastAPISurface1';
 import {BrowserRouter as BR, Routes, Route } from 'react-router-dom';
 import './App.css';
@@ -23,7 +23,6 @@ function App() {
       <BR>
         <Routes>
           <Route path='/'  element = {ret} />
-          <Route path='/para'  element = {<Op />} />
           <Route path='/table'  element = {<DispTbl />} />
         </Routes>
       </BR>
